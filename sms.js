7ca54c63ca215e3fc5072b89dfa999229777e95f
gi2019 <guyg7@mail.afeka.ac.il>
2022-11-15 23:10:34 +0200

    no tokens seen

--- sms.js
+++ sms.js
@@ -1,8 +1,8 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = 'AC39df28d29c055f877b7f9adf1feef38a';
-const authToken = '0fc88fec591cfc337881ad3328205be6';
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const TWILIO_PHONE_NUMBER = '+19787486550';
 
